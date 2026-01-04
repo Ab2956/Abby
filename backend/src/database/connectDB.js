7ed6bb6aed91b6370aabdb022b7bd5398c026c73
Adam Brows <aabrowsbrows@gmail.com>
2026-01-04 23:42:45 +0000

    env to gitignore

--- backend/src/database/connectDB.js
+++ backend/src/database/connectDB.js
@@ -1,5 +1,5 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://abrows_db:iLX5GvZZv9JbCgg8@abby-cluster.bzck6hi.mongodb.net/?retryWrites=true&w=majority&appName=abby-cluster";
+const uri = process.env.MONGODB_URI;
 
 const client = new MongoClient(uri, {
     serverApi: {
